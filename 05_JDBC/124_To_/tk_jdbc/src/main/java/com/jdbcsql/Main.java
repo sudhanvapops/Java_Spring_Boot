80f81d0e2f9aee6ab7576185809d63cc649ff260
Sudhanva <sudhanvapops@gmail.com>
2026-02-22 21:21:34 +0530

    Executed and processed Result

--- 05_JDBC/124_To_/tk_jdbc/src/main/java/com/jdbcsql/Main.java
+++ 05_JDBC/124_To_/tk_jdbc/src/main/java/com/jdbcsql/Main.java
@@ -20,6 +20,8 @@ public class Main {
         String user = dotEnv.get("DB_USER");
         String dbPassword = dotEnv.get("DB_PASSWORD");
 
+        String query = "select * from student where id = 1";
+
         // load and register
         try {
             // But modern JDBC doesn't require this anymore.
@@ -29,12 +31,38 @@ public class Main {
             // connect your app to DB
             Connection conn = DriverManager.getConnection(url, user, dbPassword);
 
-            System.out.println("Connection Established: ");
+            System.out.print("\nConnection Established: ");
             System.out.println(conn);
 
+            // create statemnt
+            Statement st = conn.createStatement();
+            // and execute and store it in rs
+            // i got it from ctrl + click and see what return type it has
+            ResultSet rs = st.executeQuery(query); // fetch
+
+            // System.out.print("Result: ");
+            // If got data returns True else False
+            // System.out.println(rs.next());
+
+            System.out.println("----Result----");
+            // while (rs.next()) {
+            // String name = rs.getString("name");
+            // System.out.println("Name: "+name);
+            // }
+
+            ResultSetMetaData meta = rs.getMetaData();
+            int cols = meta.getColumnCount();
+            while (rs.next()) {
+                for (int i = 1; i <= cols; i++) {
+                    System.out.print(rs.getObject(i) + " ");
+                }
+                System.out.println();
+            }
+
+            rs.close();
             conn.close();
 
-        } catch (SQLException  e) {
+        } catch (SQLException e) {
             System.out.println("Exeption: \n" + e.getMessage());
             e.printStackTrace();
         }
