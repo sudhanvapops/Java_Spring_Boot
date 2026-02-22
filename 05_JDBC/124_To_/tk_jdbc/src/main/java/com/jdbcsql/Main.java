9eaf5dd9ca24f5cf9de93440d38d0c9cb1f3af97
Sudhanva <sudhanvapops@gmail.com>
2026-02-22 22:38:58 +0530

    CRUD 128

--- 05_JDBC/124_To_/tk_jdbc/src/main/java/com/jdbcsql/Main.java
+++ 05_JDBC/124_To_/tk_jdbc/src/main/java/com/jdbcsql/Main.java
@@ -12,32 +12,38 @@ import io.github.cdimascio.dotenv.Dotenv;
 // close
 
 public class Main {
-    public static void main(String[] args) {
 
-        Dotenv dotEnv = Dotenv.load();
+    public static int insertIntoStudents(Connection conn, int id, int marks, String name) {
 
-        String url = dotEnv.get("DB_URL");
-        String user = dotEnv.get("DB_USER");
-        String dbPassword = dotEnv.get("DB_PASSWORD");
+        // ! CREATE
 
-        String query = "select * from student";
-
-        // load and register
+        int result = 0;
         try {
-            // But modern JDBC doesn't require this anymore.
-            // Class.forName("org.postgresql.Driver");
+            String query2 = "INSERT INTO student  VALUES (?, ?, ?)";
 
-            // create connection
-            // connect your app to DB
-            Connection conn = DriverManager.getConnection(url, user, dbPassword);
+            PreparedStatement pstmt = conn.prepareStatement(query2);
+            pstmt.setInt(1, id);
+            pstmt.setInt(2, marks);
+            pstmt.setString(3, name);
+            result = pstmt.executeUpdate();
+            System.out.println("Inserted: " + result);
 
-            System.out.print("\nConnection Established: ");
-            System.out.println(conn);
+            pstmt.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return result;
+    }
+
+    public static void readResult(Connection conn) {
+        try {
+            String query = "select * from student";
 
             // create statemnt
-            Statement st = conn.createStatement();
             // and execute and store it in rs
             // i got it from ctrl + click and see what return type it has
+            Statement st = conn.createStatement();
             ResultSet rs = st.executeQuery(query); // fetch
 
             // System.out.print("Result: ");
@@ -46,22 +52,62 @@ public class Main {
 
             System.out.println("----Result----");
             // while (rs.next()) {
-                // String name = rs.getString("name");
-                // System.out.println("Name: "+name);
-                // }
-                
-                ResultSetMetaData meta = rs.getMetaData();
-                int cols = meta.getColumnCount();
-                System.out.println("Id\tMarks\tName");
-                while (rs.next()) {
-                    for (int i = 1; i <= cols; i++) {
-                        System.out.print(rs.getObject(i) + "\t");
-                    }
-                    System.out.println();
+            // String name = rs.getString("name");
+            // System.out.println("Name: "+name);
+            // }
+
+            ResultSetMetaData meta = rs.getMetaData();
+            int cols = meta.getColumnCount();
+
+            // Printing Header Data
+            for (int i=1; i<=cols; i++){
+                System.out.print(meta.getColumnName(i)+"\t");
+            }
+            System.out.println();
+
+            while (rs.next()) {
+                for (int i = 1; i <= cols; i++) {
+                    System.out.print(rs.getObject(i) + "\t");
                 }
-                
+                System.out.println();
+            }
+
             System.out.println("--------------");
+
             rs.close();
+            st.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Dotenv dotEnv = Dotenv.load();
+
+        String url = dotEnv.get("DB_URL");
+        String user = dotEnv.get("DB_USER");
+        String dbPassword = dotEnv.get("DB_PASSWORD");
+
+        try {
+            // ! load and register
+            // But modern JDBC doesn't require this anymore.
+            // Class.forName("org.postgresql.Driver");
+
+            // ! create connection
+            // connect your app to DB
+            Connection conn = DriverManager.getConnection(url, user, dbPassword);
+
+            System.out.print("\nConnection Established: ");
+            System.out.println(conn);
+
+            // ! create
+            // insertIntoStudents(conn, 6, 90, "Adithya");
+
+            // ! Read
+            readResult(conn);
+
+            // ! Close Connection
             conn.close();
 
         } catch (SQLException e) {
