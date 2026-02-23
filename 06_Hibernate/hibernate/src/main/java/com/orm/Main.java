25fc3968c21b7ad0cf62b3d2255acc01d596bc1a
Sudhanva <sudhanvapops@gmail.com>
2026-02-23 12:50:16 +0530

    Project setup

---
+++ 06_Hibernate/hibernate/src/main/java/com/orm/Main.java
@@ -0,0 +1,9 @@
+package com.orm;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Student s1 = new Student();
+        System.out.println(s1);
+    }
+}
---
+++ 06_Hibernate/hibernate/src/main/java/com/orm/Student.java
@@ -0,0 +1,36 @@
+package com.orm;
+
+public class Student {
+
+    private int rollNo;
+    private String name;
+    private int sAge;
+
+    public int getRollNo() {
+        return rollNo;
+    }
+    public void setRollNo(int rollNo) {
+        this.rollNo = rollNo;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getsAge() {
+        return sAge;
+    }
+    public void setsAge(int sAge) {
+        this.sAge = sAge;
+    }
+    @Override
+    public String toString() {
+        return "Student { rollNo=" + rollNo + ", name=" + name + ", sAge=" + sAge + " }";
+    }
+
+    
+    
+}
