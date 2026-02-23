ba3a317e6affb1ec9a371567e7e81be961a4a5a8
Sudhanva <sudhanvapops@gmail.com>
2026-02-24 00:29:29 +0530

    Fecth Data

--- 06_Hibernate/hibernate/src/main/java/com/orm/Main.java
+++ 06_Hibernate/hibernate/src/main/java/com/orm/Main.java
@@ -1,5 +1,6 @@
 package com.orm;
 
+import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
@@ -10,35 +11,60 @@ import com.orm.config.HibernateUtil;
 
 public class Main {
 
-    // just rewriting again
-
-    public static void main(String[] args) {
-
-        // ! Configuration
-        // Configuration cfg = new Configuration();
-        // // By doing this you say add this calls as Annotated and hibernate finds it 
-        // cfg.addAnnotatedClass(com.orm.model.Student.class);
-        // cfg.configure();
-        // // Use once
-        SessionFactory sf = HibernateUtil.getSessionFactory();
-        
-        
-        // ! Data
-        Student s1 = new Student();
-        s1.setName("Akash KR 2");
-        s1.setRollNo(6);
-        s1.setsAge(21);
-        
-
-        // ! inserting to db
+    public static void insertData(Student s1,SessionFactory sf) {
         Session s = sf.openSession();
 
         Transaction tr = s.beginTransaction();
         s.persist(s1);
-        tr.commit();        
-
-        // ! Close Connection
+        tr.commit();
         s.close();
-        sf.close();
+    }
+
+    public static Student fetchData(SessionFactory sf, Integer id){
+        // Since not fetching no need of transaction
+        Session s = sf.openSession();
+        Student st = s.find(Student.class,id);
+        return st;
+
+        // Output: if id: 5
+        // Student: { rollNo: 5, name: Akash KR 2, sAge: 21 }
+    }
+
+    public static void updateData(SessionFactory sf){
+        
+    }
+
+    public static void main(String[] args) {
+
+        try (// ! Configuration
+                // Configuration cfg = new Configuration();
+                // // By doing this you say add this calls as Annotated and hibernate finds it
+                // cfg.addAnnotatedClass(com.orm.model.Student.class);
+                // cfg.configure();
+                // // Use once
+        SessionFactory sf = HibernateUtil.getSessionFactory()) {
+            // ! Data
+            Student s1 = new Student();
+            s1.setName("Akash KR 2");
+            s1.setRollNo(6);
+            s1.setsAge(21);
+
+            // ! inserting to db
+            // insertData(s1, sf);
+
+            // ! Fetching the Data
+            Student s = fetchData(sf,20);
+            System.out.print("Result: ");
+            System.out.println(s);
+
+            // ! Update Data
+
+            
+            // ! Close Connection
+            // sf.close();
+            // automatically handled by try catch
+        } catch (HibernateException e) {
+            e.printStackTrace();
+        }
     }
 }
