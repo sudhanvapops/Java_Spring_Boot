c1cf5d1ac80abb986b9be6c0b7f62c3f24df12e7
Sudhanva <sudhanvapops@gmail.com>
2026-04-17 11:35:08 +0530

    BookDAO

--- 00_projects/library-management/src/main/java/com/sudhanva/library/dao/BookDAO.java
+++ 00_projects/library-management/src/main/java/com/sudhanva/library/dao/BookDAO.java
@@ -1,5 +1,66 @@
 package com.sudhanva.library.dao;
 
-public class BookDAO {
-    
+import java.util.List;
+import java.util.Optional;
+
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+
+import com.sudhanva.library.entity.Book;
+
+
+// Unnecessory for single impleemntation
+interface BookDAOType {
+
+    public Optional<Book> findByIsbn(String isbn);
+
+    public List<Book> findByName(String name,int limit, int offset);
+
+    public void save(Book book);
+
+}
+
+public class BookDAO implements BookDAOType{
+
+    private SessionFactory sf;
+
+    public BookDAO(SessionFactory sf) {
+        this.sf = sf;
+    }
+
+    @Override
+    public Optional<Book> findByIsbn(String isbn) {
+
+        // Since it is filed that exits we use getSingleResult()
+        // But we are using OPtioanl and have to handle null case so use uniqueResultOPtional
+        Session session = sf.getCurrentSession();
+
+        Query<Book> q = session.createQuery("from Book b where b.isbn = :isbn",Book.class);
+
+        q.setParameter("isbn", isbn);
+
+        return q.uniqueResultOptional();
+        
+    }
+
+    @Override
+    public List<Book> findByName(String name,int limit, int offset) {
+        Session session = sf.getCurrentSession();
+        Query<Book> q = session.createQuery("from Book b where b.bookName  = :name",Book.class);
+
+        q.setParameter("name", name);
+        q.setMaxResults(limit);
+        q.setFirstResult(offset);
+
+        return q.getResultList();
+    }
+
+    @Override
+    public void save(Book book) {
+        Session session = sf.getCurrentSession();
+        session.persist(book);
+    }
+
 }
--- 00_projects/library-management/src/main/java/com/sudhanva/library/dao/BorrowerDAO.java
+++ 00_projects/library-management/src/main/java/com/sudhanva/library/dao/BorrowerDAO.java
@@ -11,20 +11,18 @@ import java.util.List;
 import java.util.Optional;
 
 public class BorrowerDAO {
-    
-    private SessionFactory sessionFactory;
 
+    private SessionFactory sessionFactory;
 
     public BorrowerDAO(SessionFactory sessionFactory) {
         this.sessionFactory = sessionFactory;
     }
 
-    
-    public Optional<Borrower> findByCardNumber(String cardNumber){
+    public Optional<Borrower> findByCardNumber(String cardNumber) {
 
         Session s = sessionFactory.getCurrentSession();
 
-        Query<Borrower> q = s.createQuery("FROM Borrower b WHERE b.cardNumber = :cardNumber",Borrower.class);
+        Query<Borrower> q = s.createQuery("FROM Borrower b WHERE b.cardNumber = :cardNumber", Borrower.class);
 
         q.setParameter("cardNumber", cardNumber);
 
@@ -32,26 +30,27 @@ public class BorrowerDAO {
         return q.uniqueResultOptional();
     }
 
-
-    public List<Borrower> findByName(String name){
+    // public List<Borrower> findByName(String name){
+    // making it better
+    // pagination and limit is handeled by service
+    public List<Borrower> findByName(String name, int limit, int offset) {
 
         Session s = sessionFactory.getCurrentSession();
 
-        Query<Borrower> q = s.createQuery("FROM Borrower b WHERE b.name = :name",Borrower.class);
+        Query<Borrower> q = s.createQuery("FROM Borrower b WHERE b.name = :name", Borrower.class);
 
         q.setParameter("name", name);
+        q.setMaxResults(limit);
+        q.setFirstResult(offset);
 
         return q.getResultList();
     }
 
-
-    public void save(Borrower borrower){
+    public void save(Borrower borrower) {
         Session s = sessionFactory.getCurrentSession();
         s.persist(borrower);
     }
 
     // Transaction is in service layer
 
-
-    
 }
