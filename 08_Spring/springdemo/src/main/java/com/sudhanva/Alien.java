b02396709f4a8b1cb9a497020d524f21416dac7f
Sudhanva <sudhanvapops@gmail.com>
2026-05-13 23:22:52 +0530

    First Spring Object

--- 08_Spring/springdemo/src/main/java/com/sudhanva/Alien.java
+++ 08_Spring/springdemo/src/main/java/com/sudhanva/Alien.java
@@ -1,7 +1,5 @@
 package com.sudhanva;
 
-import org.springframework.stereotype.Component;
-
 public class Alien {
     public void code(){
         System.out.println("Coding...");
--- 08_Spring/springdemo/src/main/java/com/sudhanva/App.java
+++ 08_Spring/springdemo/src/main/java/com/sudhanva/App.java
@@ -7,7 +7,7 @@ public class App {
     public static void main(String[] args) {
 
         // Making a IOC Conatiner
-        ApplicationContext context = new ClassPathXmlApplicationContext();
+        ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
 
 
         Alien obj = (Alien) context.getBean("alien");
