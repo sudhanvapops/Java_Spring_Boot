cd9a283c8f9f04800f1275710be9cedf4fbd0b5e
Sudhanva <sudhanvapops@gmail.com>
2026-05-18 09:57:52 +0530

    212

--- 12_Spring_Boot_Web/springbootweb/src/main/java/com/sudhanva/springbootweb/AddController.java
+++ 12_Spring_Boot_Web/springbootweb/src/main/java/com/sudhanva/springbootweb/AddController.java
@@ -1,9 +1,11 @@
 package com.sudhanva.springbootweb;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
-import jakarta.servlet.http.HttpServletRequest;
+// import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 
 @Controller
@@ -14,15 +16,27 @@ public class AddController {
         return "add.jsp";
     }
 
+    // @RequestMapping("/output")
+    // public String result(HttpServletRequest req,HttpSession session){
+
+
+    //     int num1 = Integer.parseInt(req.getParameter("num1"));
+    //     int num2 = Integer.parseInt(req.getParameter("num2"));
+    //     int result = num1 + num2;
+
+    //     session.setAttribute("result", result);
+
+    //     return "output.jsp";
+    // }
+
+    // Spring Boot Way
+
     @RequestMapping("/output")
-    public String result(HttpServletRequest req,HttpSession session){
+    public String result(@RequestParam("num1") int a, @RequestParam("num2") int b,Model model){
 
+        int result = a + b;
 
-        int num1 = Integer.parseInt(req.getParameter("num1"));
-        int num2 = Integer.parseInt(req.getParameter("num2"));
-        int result = num1 + num2;
-
-        session.setAttribute("result", result);
+        model.addAttribute("result", result);
 
         return "output.jsp";
     }
