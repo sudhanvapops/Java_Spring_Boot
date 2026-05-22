f5f603bfe57853a6377e64a6a3e0e3ac6d4500a9
Sudhanva <sudhanvapops@gmail.com>
2026-05-22 11:40:22 +0530

    Started for 14 job app

---
+++ 00_projects/14_Job_App/jobapp/src/test/java/com/sudhanva/jobapp/JobappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sudhanva.jobapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
