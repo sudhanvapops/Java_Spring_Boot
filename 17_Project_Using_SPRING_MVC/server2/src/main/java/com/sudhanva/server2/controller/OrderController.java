71bb164123aabbf659c73b0100d6d4eb88bc2c33
Sudhanva <sudhanvapops@gmail.com>
2026-06-18 11:51:47 +0530

    272 Controller

---
+++ 17_Project_Using_SPRING_MVC/server2/src/main/java/com/sudhanva/server2/controller/OrderController.java
@@ -0,0 +1,42 @@
+package com.sudhanva.server2.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.sudhanva.server2.Model.dto.OrderResponse;
+import com.sudhanva.server2.service.OrderService;
+
+@RestController
+@RequestMapping("/api")
+@CrossOrigin(origins = "http://localhost:5173")
+public class OrderController {
+    
+    @Autowired
+    OrderService orderService;
+
+    @GetMapping("/orders")
+    public ResponseEntity<List<OrderResponse>> getAllOrders(){
+        List<OrderResponse> response = orderService.getAllOrderResponses();
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(response);
+    }
+    
+    @PostMapping("/orders/place")
+    public ResponseEntity<OrderResponse> placeOrder(){
+        OrderResponse orderResponse = orderService.placeOrder();
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(orderResponse);
+    }
+
+}
---
+++ 17_Project_Using_SPRING_MVC/server2/src/main/java/com/sudhanva/server2/service/OrderService.java
@@ -0,0 +1,20 @@
+package com.sudhanva.server2.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.sudhanva.server2.Model.dto.OrderResponse;
+
+@Service
+public class OrderService {
+
+    public OrderResponse placeOrder() {
+        return null;
+    }
+
+    public List<OrderResponse> getAllOrderResponses() {
+        return null;
+    }
+    
+}
