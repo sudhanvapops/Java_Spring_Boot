22563f49785d024887bf8b2f6e117cbf4f901aeb
Sudhanva <sudhanvapops@gmail.com>
2026-06-24 01:42:19 +0530

    Added Swagger

--- 17_Project_Using_SPRING_MVC/server2/src/main/java/com/sudhanva/server2/Server2Application.java
+++ 17_Project_Using_SPRING_MVC/server2/src/main/java/com/sudhanva/server2/Server2Application.java
@@ -3,6 +3,7 @@ package com.sudhanva.server2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class Server2Application {
 
---
+++ 17_Project_Using_SPRING_MVC/server2/src/test/java/com/sudhanva/server2/service/OrderServiceTest.java
@@ -0,0 +1,16 @@
+package com.sudhanva.server2.service;
+
+import org.junit.jupiter.api.Test;
+
+public class OrderServiceTest {
+
+    @Test
+    void testGetAllOrderResponses() {
+        System.out.println("Testing Order");
+    }
+
+    @Test
+    void testPlaceOrder() {
+
+    }
+}
