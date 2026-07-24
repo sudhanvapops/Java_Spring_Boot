0de2a345444341ca42a187919ee30c0ca57d7058
Sudhanva <sudhanvapops@gmail.com>
2026-07-24 09:12:30 +0530

    281

---
+++ 17_Project_Using_SPRING_MVC/server2/src/main/java/com/sudhanva/server2/aop/LoggingAspect.java
@@ -0,0 +1,16 @@
+package com.sudhanva.server2.aop;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class LoggingAspect {
+
+    private static final Logger LOGGER = 
+        LoggerFactory.getLogger(LoggingAspect.class);
+
+    
+    public void logMethodCall(){
+        LOGGER.info("Method Called: ");
+    }
+
+}
