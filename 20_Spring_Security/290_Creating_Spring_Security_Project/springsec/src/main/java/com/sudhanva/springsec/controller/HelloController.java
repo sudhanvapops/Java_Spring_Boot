c39cad8ad644fa2f04ecce26526b524cef77fa04
Sudhanva <sudhanvapops@gmail.com>
2026-07-25 23:47:32 +0530

    CSRF

--- 20_Spring_Security/290_Creating_Spring_Security_Project/springsec/src/main/java/com/sudhanva/springsec/controller/HelloController.java
+++ 20_Spring_Security/290_Creating_Spring_Security_Project/springsec/src/main/java/com/sudhanva/springsec/controller/HelloController.java
@@ -1,5 +1,6 @@
 package com.sudhanva.springsec.controller;
 
+import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +13,11 @@ public class HelloController {
     @GetMapping("/hello")
     public String greet(HttpServletRequest request){
         return "Hello World Session Id: "+request.getSession().getId();
+    }
+
+    @GetMapping("/csrf")
+    public CsrfToken getCsrfToken(HttpServletRequest req){
+        return (CsrfToken) req.getAttribute("_csrf");
     }
 
 }
